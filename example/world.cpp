f3bee4ccef0f88136d8192f91ca957cd627514f8
Taj Jethwani-Keyser <wolfiejetkey@gmail.com>
2026-04-22 14:02:10 -0400

    hello world example

---
+++ example/world.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <print>
+
+#include "client.h"
+
+extern "C" void entry(int cid) {
+    auto* msg = static_cast<char*>(std::malloc(32));
+    std::snprintf(msg, 32, "world");
+    sas::put<char, [](char* p) { std::free(p); }>("greeting", msg);
+
+    auto h = sas::get<char>("greeting");
+    std::println("{}: {}", cid, h ? h.get() : "(null)");
+}
